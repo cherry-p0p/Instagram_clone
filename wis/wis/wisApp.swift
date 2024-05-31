1095221d4265b803d4346f44b989f246e14db33b
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2024-05-31 18:17:11 +0900

    week8

---
+++ wis/wis/wisApp.swift
@@ -0,0 +1,17 @@
+//
+//  wisApp.swift
+//  wis
+//
+//  Created by 이슬기 on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct wisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
